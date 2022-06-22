568b887bdd2c4c8a937514701f36cb74a6c648ec
Mennapika <mennana3im@gmail.com>
2022-06-22 19:49:48 +0200

    create handle resize function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,31 @@
+import { useEffect, useState } from 'react';
 import { Hero } from './Components/Hero';
 import { NavBar } from './Components/NavBar';
 import logo from './logo.svg';
-
+import React from 'react';
+import { NavBarMobile } from './Components/NavBarMobile';
+import { HeroMobile } from './Components/HeroMobile';
 function App() {
+ const [isMobile,setIsMobile]= useState(false);
+ const handleResize=()=>{
+  if(window.innerWidth<830){
+    setIsMobile(true);
+  }else{
+    setIsMobile(false)
+  }
+ }
+
+ useEffect(()=>{
+  window.addEventListener('resize',handleResize);
+  return()=>window
+ })
+ 
   return (
-  <div>
-   <NavBar/>
-   <Hero/>
-  </div>
+    <div>
+    {
+      isMobile? <div><NavBarMobile/> <HeroMobile/></div>: <div><NavBar/> <Hero/></div>
+    }
+    </div>
   );
 }
 
